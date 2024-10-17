784c198c14982f32ccc108395ce25f75eb1f3f55
Camila Silva <cf.silv466@gmail.com>
2024-10-17 14:06:48 -0300

    FIX: "adjust image path and benefics.json file deleted"

--- src/components/benefits/card/index.jsx
+++ src/components/benefits/card/index.jsx
@@ -1,18 +1,13 @@
 import PropTypes from "prop-types";
 import { useDarkMode } from "../../../model/darkMode";
 
-const Card = ({ title, text, icon }) => {
-
+const Card = ({ title, text, src, alt }) => {
   const { darkMode } = useDarkMode();
 
   return (
     <div className="dark:bg-purple-2 shadow-cardShadow drop-shadow-none rounded-lg flex flex-col gap-2 p-8   w-medium h-extraSmall lg:gap-4 lg:h-larger">
       <h2 className="text-base font-bold flex gap-2.5 font-mulish dark:text-gray-2 lg:text-lg lg:gap-1.5">
-        <img
-          src={`./src/public/assets/icons/${icon}`}
-          alt="Icone do cartão"
-          className="h-6"
-        />
+        <img src={src} alt={alt} className="h-6" />
         {title}
       </h2>
       <p className="text-xs font-normal leading-mobile-medium dark:text-purple-3 font-mulish lg:text-desktop-extraMini lg:leading-desktop-larger">
@@ -27,5 +22,6 @@ export default Card;
 Card.propTypes = {
   title: PropTypes.string,
   text: PropTypes.string,
-  icon: PropTypes.string,
+  src: PropTypes.string,
+  alt: PropTypes.string,
 };
