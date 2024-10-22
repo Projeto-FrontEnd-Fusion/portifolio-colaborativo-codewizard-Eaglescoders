2759389fcc95931916ca07c9140c32b061b1bfdf
claudio silva <claudiosilva.one@gmail.com>
2024-10-22 15:36:59 -0300

    refact : chage data object

--- src/service/methods/get.js
+++ src/service/methods/get.js
@@ -1,7 +1,7 @@
 export const getAllMembers = async (api) => {
   try {
     const response = await api.get('api/EagleCoders')
-    return response.data.people
+    return response.data
   } catch (error) {
     console.error('Falha ao Acessar Rota', error)
     throw error
