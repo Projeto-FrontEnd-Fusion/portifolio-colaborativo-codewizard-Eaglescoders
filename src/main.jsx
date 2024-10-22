09d35e78477c9a69e385e2385736053a1fe33e5b
claudio silva <claudiosilva.one@gmail.com>
2024-10-22 12:43:21 -0300

    refactor: reorganize import structure in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { HeroSection } from "./components/hero";
-import { Footer } from "./components/rodape";
-import { AboutProject } from "./components/about-project";
-import Benefits from "./components/benefits";
-import Comments from "./components/coments";
-import { Header } from "./components/cabecalho";
-import {Team} from "./components/team";
-import LingAndTech from "./components/ling-and-tech/LingAndTech";
+import { App } from "./app";
 
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <main className="flex flex-col bg-white-2 dark:bg-purple-1"> {/* Não mexer aqui por favor! */}
-      <Header />
-      <HeroSection />
-      <Benefits />
-      <LingAndTech />
-      <Team />
-      <AboutProject />
-      <Comments />
-      <Footer />
-    </main>
-);
-
-
-
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
