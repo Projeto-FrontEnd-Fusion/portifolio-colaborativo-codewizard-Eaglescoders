b1a9afacf9b09d5d07c3dab1a3552e56f8cb0a2b
claudio silva <claudiosilva.one@gmail.com>
2024-10-22 12:46:51 -0300

    feat: add TanStack Query for query management

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./app";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const query = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={query}>
+    <App />
+  </QueryClientProvider>
+);
