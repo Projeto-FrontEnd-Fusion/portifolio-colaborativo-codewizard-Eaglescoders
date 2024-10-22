55430b3d4897ea159ba44b2d7f22ba42c4139b4c
claudio silva <claudiosilva.one@gmail.com>
2024-10-22 12:40:38 -0300

    refactor: reorganize import structure in main.jsx

---
+++ src/app.jsx
@@ -0,0 +1,22 @@
+import { HeroSection } from "./components/hero";
+import { Footer } from "./components/rodape";
+import { AboutProject } from "./components/about-project";
+import Benefits from "./components/benefits";
+import Comments from "./components/coments";
+import { Header } from "./components/cabecalho";
+import {Team} from "./components/team";
+import LingAndTech from "./components/ling-and-tech/LingAndTech";
+export const App = () => {
+  return (
+    <main className="flex flex-col bg-white-2 dark:bg-purple-1">
+      <Header />
+      <HeroSection />
+      <Benefits />
+      <LingAndTech />
+      <Team />
+      <AboutProject />
+      <Comments />
+      <Footer />
+    </main>
+  );
+};
