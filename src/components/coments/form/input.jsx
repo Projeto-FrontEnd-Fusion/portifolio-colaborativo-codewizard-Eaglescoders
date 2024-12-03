d05c53212de3e3152738767fc3799b43092eadaa
g42puts <gilmar.neo@gmail.com>
2024-12-03 01:46:09 -0300

    feat/fix: add proptypes and incremented file structure

--- src/components/coments/form/input.jsx
+++ src/components/coments/form/input.jsx
@@ -1,19 +1,31 @@
-export const InputForm = ({
-  inputType, placeholderContent, inputValue , register, onBlurFunction
-}) =>{
-  return(
-    <>
-       <input
-              type={inputType}
-              placeholder={placeholderContent}
-              value={inputValue || ""}
-              className="w-full bg-inherit h-9 rounded-lg border border-blue-1 
-              p-2.5 outline-none text-purple-1 dark:text-white-1 font-inter 
-              lg:h-14 lg:px-8 lg:max-w-min"
-              {...register}
-              onBlur={onBlurFunction}
-            />
-    </>
-  )
-}
+import PropTypes from 'prop-types';
 
+export const InputForm = ({
+  inputType,
+  placeholderContent,
+  inputValue,
+  register,
+  onBlurFunction,
+}) => {
+  return (
+    <>
+      <input
+        type={inputType}
+        placeholder={placeholderContent}
+        value={inputValue || ''}
+        className='w-full bg-inherit h-9 rounded-lg border border-blue-1 
+              p-2.5 outline-none text-purple-1 dark:text-white-1 font-inter 
+              lg:h-14 lg:px-8 lg:max-w-min'
+        {...register}
+        onBlur={onBlurFunction}
+      />
+    </>
+  );
+};
+InputForm.propTypes = {
+  inputType: PropTypes.string,
+  placeholderContent: PropTypes.string,
+  inputValue: PropTypes.string,
+  register: PropTypes.object,
+  onBlurFunction: PropTypes.func,
+};
--- src/utils/textformatter.js
+++ src/utils/textformatter.js
@@ -1,5 +1,4 @@
-
 export const textFormater = (term) => {
-  const termFormater= term.trim().replace(/\s+/g, "");
-  return termFormater
- } 
+  const termFormater = term.trim().replace(/\s+/g, '');
+  return termFormater;
+};
